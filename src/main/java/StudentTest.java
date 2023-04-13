bd529d76c150258ef0036ed9c44dacb8ef64925c
Minh Nguyen <quangminh.nguyen8989@gmail.com>
2023-04-13 14:11:13 -0500

    exercise complete

---
+++ src/main/java/StudentTest.java
@@ -0,0 +1,27 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class StudentTest {
+    public static void main(String[] args) {
+
+        Scanner objectScanner = new Scanner(System.in);
+        System.out.print("Enter Something: ");
+        String userInput = objectScanner.nextLine();
+
+        System.out.println("You Entered: " + userInput);
+
+        if (StringUtils.isNumeric(userInput)){
+            System.out.println(userInput + " is a number");
+        } else {
+            System.out.println(userInput + " is NOT a number");
+        }
+
+        String flippedCase = StringUtils.swapCase(userInput);
+        System.out.println("Flipped Case: " + flippedCase);
+
+        String reversed = StringUtils.reverse(userInput);
+        System.out.println("Reversed: " + reversed);
+
+    }
+}
